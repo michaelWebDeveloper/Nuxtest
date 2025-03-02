8c76636aed204818b3c31fbeabe8cf3f02004a7c
michaelWebDeveloper <mikle.web.developer@gmail.com>
2025-03-02 21:04:03 +0300

    hopes and dreams

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>Microphone Drop Like It's Hot BIEEAAAATTTCH!!</h1>
-  <h1>O'G SPOKEN !</h1>
-  <h1>WHO IS THE MAN</h1>
+  <div class="main_hope">
+    <h1>soon there going to be great things here</h1>
+    <h3>so we hope...</h3>
+  </div>
 </template>
+
+<style scoped>
+  .main_hope{
+    background-color: honeydew;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #333;
+    text-align: center;
+    padding: 30px 0;
+  }
+</style>
